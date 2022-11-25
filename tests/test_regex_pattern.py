3b06bb0d4ed14fe4c176eff5f15fd7d0e1cf69c4
Macarena Fernandez Urquiza <murquiza@uma-health.com>
2022-11-25 18:04:30 +0100

    [test] add regex pattern function

---
+++ tests/test_regex_pattern.py
@@ -0,0 +1,14 @@
+def test_given_texts_when_find_pattern_runs_then_returns_expected_matches_list():
+    pass
+def test_given_texts_and_matches_when__highlight_match_runs_then_returns_expected_highlighted_text():
+    pass
+def test_given_texts_and_matches_when_highlight_match_runs_then_returns_expected_message():
+    pass
+def test_given_texts_and_matches_when__generate_file_output_runs_then_returns_expected_text():
+    pass
+def test_given_texts_and_matches_when_save_file_runs_then_returns_None():
+    pass
+
+
+
+
